8346fa7d38b810f9c1dfe06c1e7d7b85c9736d06
Jeferson Guardezi <jguardezi@ext.vectoritcgroup.com>
2019-07-13 17:25:33 -0300

    adicionei os modulos do firebase

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -9,18 +9,25 @@ import { StatusBar } from '@ionic-native/status-bar/ngx';
 import { AppComponent } from './app.component';
 import { AppRoutingModule } from './app-routing.module';
 
+import { AngularFireModule } from '@angular/fire';
+import { AngularFirestoreModule, FirestoreSettingsToken } from '@angular/fire/firestore';
+import { firebase } from '../environments/environment';
+
 @NgModule({
   declarations: [AppComponent],
   entryComponents: [],
   imports: [
     BrowserModule,
     IonicModule.forRoot(),
-    AppRoutingModule
+    AppRoutingModule,
+    AngularFireModule.initializeApp(firebase),
+    AngularFirestoreModule
   ],
   providers: [
     StatusBar,
     SplashScreen,
-    { provide: RouteReuseStrategy, useClass: IonicRouteStrategy }
+    { provide: RouteReuseStrategy, useClass: IonicRouteStrategy },
+    { provide: FirestoreSettingsToken, useValue: {} }
   ],
   bootstrap: [AppComponent]
 })
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -6,6 +6,16 @@ export const environment = {
   production: false
 };
 
+export const firebase = {
+  apiKey: 'AIzaSyCIZOBl2vH8mAGSkh1qTunIBrZ7uerk49o',
+  authDomain: 'divideaconta-dev.firebaseapp.com',
+  databaseURL: 'https://divideaconta-dev.firebaseio.com',
+  projectId: 'divideaconta-dev',
+  storageBucket: 'divideaconta-dev.appspot.com',
+  messagingSenderId: '942469716149',
+  appId: '1:942469716149:web:01d59e5601abac4c'
+};
+
 /*
  * For easier debugging in development mode, you can import the following file
  * to ignore zone related error stack frames such as `zone.run`, `zoneDelegate.invokeTask`.
