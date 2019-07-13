e0e57f26923a6f0429b00fc7e06eb19198d332bb
Jeferson Guardezi <jguardezi@ext.vectoritcgroup.com>
2019-07-13 18:02:56 -0300

    firebase cli

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -1,4 +1,8 @@
 import { Component } from '@angular/core';
+import { AngularFirestore, AngularFirestoreCollection, AngularFirestoreDocument, DocumentReference } from '@angular/fire/firestore';
+import { map, take } from 'rxjs/operators';
+import { Observable } from 'rxjs';
+
 
 @Component({
   selector: 'app-home',
@@ -6,7 +10,24 @@ import { Component } from '@angular/core';
   styleUrls: ['home.page.scss'],
 })
 export class HomePage {
+  private eventCollection: AngularFirestoreCollection<any>;
+  private events: Observable<any[]>;
+  constructor(
+    private afs: AngularFirestore) {
+    this.eventCollection = this.afs.collection<any>('users/5511957925512/events/');
+    this.events = this.eventCollection.snapshotChanges().pipe(
+      map(actions => {
+        return actions.map(a => {
+          const data = a.payload.doc.data();
+          const id = a.payload.doc.id;
+          return { id, ...data };
+        });
+      })
+    );
+  }
 
-  constructor() {}
-
+  addNewEvent(){
+    this.eventCollection.add({data: new Date()});
+    console.log('add new event');
+  }
 }
